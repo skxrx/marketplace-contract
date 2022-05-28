48afa75ee2f6bef341929fe1080efc8a21fd64aa
Semyon Zhulikov <51035894+skxrx@users.noreply.github.com>
2022-05-28 14:24:31 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 const contractAddress = '0xf7C1FAEC15838c1A1e80fE69544A350612735AEE'
 const nftAddress = '0x8D1F28AA789055ea598e25B9fd36679eCB24E1F9'
-const ownerAddress = ''
+const ownerAddress = '0xbe82fa5E29424659624695F71E4ee7dDa5513c23'
 
 export { contractAddress, nftAddress, ownerAddress }
